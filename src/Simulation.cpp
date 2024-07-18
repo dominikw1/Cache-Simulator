0ae4fdd3982d7d87350d66c33b2bf8d8f873f3d9
dominik.m.weber <dominik.m.weber@gmail.com>
2024-07-18 17:29:03 +0200

    fix correct detection of tiemout

--- src/Simulation.cpp
+++ src/Simulation.cpp
@@ -182,15 +182,16 @@ Result run_simulation_extended(uint32_t cycles, unsigned int cacheLines, unsigne
         sc_trace(trace, instrRamReadySignal, "instrRamReadySignal");
     }
 
-   // sc_set_time_resolution(1, SC_NS);
-    sc_start(sc_time::from_value(cycles*1000ull)); // from_value takes pico-seconds and each of our cycles is a NS
+    // sc_set_time_resolution(1, SC_NS);
+    sc_start(sc_time::from_value(cycles * 1000ull)); // from_value takes pico-seconds and each of our cycles is a NS
 
     if (tracefile != NULL) {
         sc_close_vcd_trace_file(trace);
     }
 
+    std::cout << pcSignal << " " << numRequests << std::endl;
     return Result{
-        pcSignal >= numRequests
+        pcSignal >= numRequests - 1
             ? cpu.getElapsedCycleCount()
             : SIZE_MAX,      // TODO: What if last request is not finished yet, but pc is already at numRequests
         dataCache.missCount, // TODO: What if a reuqest is stopped in the middle of processing, should the MISS/HIT be
