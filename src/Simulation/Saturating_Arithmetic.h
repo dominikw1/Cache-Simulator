c32d68b253e0390a3eb44a5d5ae436a74f4cd20e
dominik.m.weber <dominik.m.weber@gmail.com>
2024-07-20 17:11:10 +0200

    keep workign on gates

--- src/Simulation/Saturating_Arithmetic.h
+++ src/Simulation/Saturating_Arithmetic.h
@@ -3,18 +3,27 @@
 
 // Try to emulate C++26 add_sat etc.
 
-template <typename T> constexpr inline T addSatUnsigned(T a, T b) noexcept {
+template <typename T> constexpr inline auto addSatUnsigned(T a, T b) noexcept {
     T c;
     if (!__builtin_add_overflow(a, b, &c))
         return c;
     return std::numeric_limits<T>::max();
 }
 
-template <typename T> constexpr inline T mulSatUnsigned(T a, T b) noexcept {
+template <typename T, typename... U> constexpr inline auto addSatUnsignedMany(T a, U... nums) {
+    return addSatUnsigned(a, addSatUnsignedMany(nums...));
+}
+
+template <typename T> constexpr inline auto mulSatUnsigned(T a, T b) noexcept {
     T c;
     if (!__builtin_mul_overflow(a, b, &c))
         return c;
     return std::numeric_limits<T>::max();
+}
+template <typename T> constexpr inline auto mulSatUnsigned(T a) noexcept { return a; }
+
+template <typename T, typename... U> constexpr inline auto mulSatUnsigned(T a, U... nums) noexcept {
+    return mulSatUnsigned(a, mulSatUnsigned(nums...));
 }
 
 template <typename T> constexpr inline T powSatUnsigned(T a, T b) noexcept {
